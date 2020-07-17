2732f197f0568223764114df1e28392e984b5de0
khush <k319kr@gmail.com>
2020-07-17 09:53:12 +0530

    Add files via upload

---
+++ day2.js
@@ -0,0 +1,11 @@
+var name=prompt("Enter your name");
+document.write(name);
+
+//Quetion-3
+var age= prompt("enter the age");
+if (age>21){
+    document.write("can Drink")
+
+} else{
+    document.write("cannot drink")
+}
